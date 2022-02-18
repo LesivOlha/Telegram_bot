c448d96c9a84155de096ab63c3257d13b8250e0a
LesivOlha <lesivolga98@gmail.com>
2022-02-18 17:07:57 +0200

    create bot2

---
+++ bot 2.py
@@ -0,0 +1,63 @@
+import json
+import requests
+
+
+class TelegramBot:
+    def __init__(self, chat_id, token):
+        self.chat_id = chat_id
+        self.token = token
+
+    def get_json(self, url):
+        data = requests.get(url)
+        json_data = json.loads(data.text)
+        return json_data
+
+    def send_message(self, currency, from_id):
+        json_rates = self.get_json('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5')
+        text = ''
+        for i in json_rates:
+            if currency == 'USD' and i['ccy'] == 'USD':
+                text = f'Курс долара: \n {i["ccy"]} \n {i["buy"]} \n {i["sale"]}'
+                break
+            elif currency == 'EUR' and i['ccy'] == 'EUR':
+                text = f'Курс євро: \n {i["ccy"]} \n {i["buy"]} \n {i["sale"]}'
+                break
+            elif currency == 'RUR' and i['ccy'] == 'RUR':
+                text = f'Курс російського рубля: \n {i["ccy"]} \n {i["buy"]} \n {i["sale"]}'
+                break
+        telegram_url = f'https://api.telegram.org/bot{self.token}/sendMessage?chat_id={from_id}&text={text}'
+        self.get_json(telegram_url)
+
+    def first_message(self, from_id):
+        only_first_message = 'Привіт, я знаю який зараз курс валют. А ти?' + '\n' + 'Введіть назву валюти: USD , EUR, RUR.'
+        telegram_url = f'https://api.telegram.org/bot{self.token}/sendMessage?chat_id={from_id}&text={only_first_message}'
+        self.get_json(telegram_url)
+
+    def get_updates(self):
+        update_ids = []
+        update_url = f'https://api.telegram.org/bot{self.token}/getUpdates'
+        while True:
+            updates = self.get_json(update_url)
+            last_message = updates['result'][-1]
+            if last_message.get('message'):
+                from_id = last_message['message']['from']['id']
+                if not last_message['update_id'] in update_ids:
+                    update_ids.append(last_message['update_id'])
+                    if last_message['message'].get('text'):
+                        optimal = last_message['message']['text'].upper()
+                        if optimal in ['USD', 'EUR', 'RUR']:
+                            self.send_message(optimal, from_id)
+                        elif last_message['message']['text'] == '/start':
+                            self.first_message(from_id)
+                        else:
+                            text = 'Введіть назву валюти: USD , EUR, RUR.'
+                            telegram_url = f'https://api.telegram.org/bot{self.token}/sendMessage?chat_id={from_id}&text={text}'
+                            self.get_json(telegram_url)
+            else:
+                update_ids.append(last_message['update_id'])
+                if not last_message['update_id'] in update_ids:
+                    self.first_message(last_message['my_chat_member']['from']['id'])
+
+
+activation_bot = TelegramBot('732897762', '5252793321:AAG3wJoRVlXo-SJ12trF-edZ3VIENPpIFjI')
+activation_bot.get_updates()
